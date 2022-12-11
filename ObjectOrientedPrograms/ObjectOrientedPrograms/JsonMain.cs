ea50bf5e0587057fb3142ab836ceff7c187a35f8
FIRST_NAME LAST_NAMEsanketppatil99 <sanketppatil99@gmail.com>
2022-12-11 22:31:16 +0530

    JSON Inventory Data Management of Rice, Pulses and Wheats

---
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/JsonMain.cs
@@ -0,0 +1,35 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Schema;
+
+namespace ObjectOrientedPrograms
+{
+    internal class JsonMain
+    {
+        model Inventory = new model();
+
+        List<model> InventoryList = new List<model>();
+
+        public void convert(string jFilePath)
+        {
+            using (StreamReader reader = new StreamReader(jFilePath))
+            {
+                var json = reader.ReadToEnd();
+                var items = JsonConvert.DeserializeObject<List<model>>(json);
+                Console.WriteLine("Name weight Price Total value");
+
+                foreach (var item in items)
+                {
+                    double value = item.price * item.weight;
+
+                    Console.WriteLine(item.name + " " + item.weight + " " + item.price + " " + value);
+                }
+            }
+        }
+    }
+}
+
---
+++ ObjectOrientedPrograms/ObjectOrientedPrograms/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace ObjectOrientedPrograms
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            ObjectOrientedPrograms.JsonMain json = new ObjectOrientedPrograms.JsonMain();
+            json.convert("D:\\231\\Assignment11&12_\\ObjectOrientedPrograms_JSONFile\\ObjectOrientedPrograms\\ObjectOrientedPrograms\\Inventory.json");
+        }
+    }
+}
